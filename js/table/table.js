dfc626b3a9dd73807129f3feda0562782e74700a
Chrilleweb <christian.munknissen@gmail.com>
2023-11-25 13:36:42 +0100

    view user

--- js/table/table.js
+++ js/table/table.js
@@ -1,42 +1,41 @@
 export default function createTable(data) {
-    // Ensure data is not empty
     if (!data || data.length === 0) {
         console.error("No data provided to create table");
         return;
     }
 
-    // Get the main container
     const mainContainer = document.getElementById("main-container");
     if (!mainContainer) {
         console.error("Main container not found");
         return;
     }
 
-    // Create table and its elements
     const table = document.createElement("table");
     table.className = "custom-table";
 
     const header = table.createTHead();
     const headerRow = header.insertRow();
 
-    // Create column headers
-    Object.keys(data[0]).forEach(key => {
+    const desiredFields = ['firstname', 'middlename', 'lastname', 'email', 'userType'];
+
+    // Create column headers for desired fields
+    desiredFields.forEach(field => {
         const th = document.createElement("th");
-        th.innerHTML = key;
+        th.innerHTML = field;
         headerRow.appendChild(th);
     });
 
-    // Create table body and rows
     const tbody = document.createElement("tbody");
+
     data.forEach(rowData => {
         const row = tbody.insertRow();
-        Object.values(rowData).forEach(value => {
+        desiredFields.forEach(field => {
             const cell = row.insertCell();
-            cell.innerHTML = value;
+            cell.innerHTML = rowData[field] || ''; // Handle undefined or missing values
         });
     });
 
-    // Append the tbody to the table and the table to the main container
     table.appendChild(tbody);
     mainContainer.appendChild(table);
 }
+
